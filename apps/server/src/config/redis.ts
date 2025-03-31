a4383ef674ef1a71c5fb67f112027089d83bd05a
Brooklyn7x <126967831+Brooklyn7x@users.noreply.github.com>
2025-04-01 04:43:50 +0530

    Update Redis connection variable for consistency

--- apps/server/src/config/redis.ts
+++ apps/server/src/config/redis.ts
@@ -2,7 +2,7 @@ import Redis from "ioredis";
 import logger from "./logger";
 import "dotenv/config";
 
-const redisClient = new Redis(process.env.REDIS_URI as string);
+const redisClient = new Redis(process.env.REDIS_URL as string);
 
 redisClient.on("connect", () => logger.info("Connected to Redis"));
 redisClient.on("error", (err) => logger.error(`Redis Client Error: ${err}`));
